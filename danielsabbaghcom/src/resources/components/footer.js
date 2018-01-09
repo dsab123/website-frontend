b40e0b32996c2f9c730e6a380cc0c58ab8700328
dsab123 <dsabbaghumd@gmail.com>
2018-01-08 20:36:42 -0500

    played w/ media queries, base footer skeleton

--- danielsabbaghcom/src/resources/components/footer.js
+++ danielsabbaghcom/src/resources/components/footer.js
@@ -1,3 +1,8 @@
 export class Footer {
-    
+    constructor() {
+        // get these from some config or something
+        this.blurbText = "This website is my effort at tracking my professional growth as a software developer. I've recently become interested in web stuff. Oh, I also post theological stuff from time to time too. ";
+        this.email = "sampleEmail@email.com";
+    }
+
 }
