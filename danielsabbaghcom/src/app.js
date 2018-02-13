57cca4c9c873ced8cd8b809ab9696310b87a1c5d
dsab123 <dsabbaghumd@gmail.com>
2018-02-12 19:59:38 -0500

    added beginnings of postApi

--- danielsabbaghcom/src/app.js
+++ danielsabbaghcom/src/app.js
@@ -1,6 +1,7 @@
 export class App {
   constructor() {
     this.message = 'Hello World! under this is the router view';
+    this.title = "Daniel Sabbagh!";
   }
   
   configureRouter(config, router) {
@@ -9,19 +10,22 @@ export class App {
     config.map([
       {
         route: ['', 'home'], 
-        name: 'home', 
+        name: 'home',
+        nav: true, 
         moduleId: 'resources/components/home', 
         title: 'Home'
       },
       {
         route: 'about',
         name: 'about',
+        nav: true,
         moduleId: 'resources/components/about',
         title: 'About'
       },
       {
         route: 'post',
         name: 'post',
+        nav: true,
         moduleId: 'resources/components/post',
         title: 'Post' // how to get post name in here? might have to do in the post logic
       }
--- danielsabbaghcom/src/resources/api/postApi.js
+++ danielsabbaghcom/src/resources/api/postApi.js
@@ -1,17 +1,38 @@
 import {noView} from 'aurelia-framework';
+import 'fetch';
+//import {HttpClient} from 'aurelia-fetch-client';
 
 @noView
 export class PostApi {
     constructor() {
+        //this.httpClient = new HttpClient();
+    }
 
-
-
+    activate(urlParams, routeMap, navigationInstruction) {
+        console.log('activate of post called!');
     }
 
 
-    fetchPost(postId) {
-        console.log('fetchin post ' + postId + " ...");
+    fetchBlogPost(blogPostId) {
+        // until I design a more intelligent post retrieval
+        // scheme (for which I'll move posts from the static
+        // assets part of this project to a database or something)
+        // I'll just fetch the file from /static or something
 
-        // fetch static file containing the post id
+        console.log('fetchin post ' + blogPostId + " ...");
+
+        // create post filename
+        let blogPostContents = 'lorem ipsum yadda yadda yadda';
+        //let blogPostContent = this.retrieveBlogPost(blogPostId);
+    }
+
+    retrieveBlogPost(blogPostId) {
+        /*
+        this.httpClient.fetch('https://baconipsum.com/api/?type=meat-and-filler')
+            .then(response => response.json())
+            .then(data => {
+                console.log(data.description);
+            });
+            */
     }
 }
