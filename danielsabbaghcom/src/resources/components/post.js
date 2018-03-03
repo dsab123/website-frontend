13537247618a38a790390804703bbbdc3f93eb85
dsab123 <dsabbaghumd@gmail.com>
2018-03-02 23:48:55 -0500

    added related posts functionality

--- danielsabbaghcom/src/resources/components/post.js
+++ danielsabbaghcom/src/resources/components/post.js
@@ -1,16 +1,22 @@
 import {inject} from 'aurelia-framework';
 import {PostApi} from '../api/postApi';
+import {BlogPostBlurb} from '../models/blogPostBlurb';
 
 @inject(PostApi)
 export class Post {
 
     constructor(PostApi) {
         this.postApi = PostApi;
-        this.postId = '1';
 
+        // properties for main post
         this.postTitle = null;
         this.postContents = null;
         this.postTags = null;
+
+        // properties for related posts by tag
+        this.relatedPosts = [];
+        this.selectedRelatedPost = null;
+        this.numberOfRelatedPostBlurbsToFetch = 5;
     }
 
     activate(urlParams, routeMap, navigationInstruction) {
@@ -23,12 +29,15 @@ export class Post {
 
     }
 
+    // TODO probs don't need this async prefix
     async getPostContents(postId) {
         if (!postId) {
             postId = this.getDefaultPostId();
         }
 
-        this.postApi.retrieveBlogPost(postId).then((data) => {            
+        this.postApi.retrieveBlogPost(postId).then((data) => {
+
+            // TODO check if data instance of blogPost
             this.postContents = data.content;
             this.postTitle = data.title;
             this.postTags = data.tags.split(',');
@@ -41,5 +50,21 @@ export class Post {
 
 
     async getRelatedPostsByTag(postTag) {
+        this.selectedRelatedPost = postTag;
+
+        // TODO implement with this.numberOfRelatedPostBlurbsToFetch
+        this.postApi.retrieveBlogPostBlurbsByTag(postTag).then((data) => {
+            if (!Array.isArray(data)) {
+                console.log('ERROR: data is not array');
+            }
+
+            for (let i = 0; i < data.length; i++) {
+                if (!data[i] instanceof BlogPostBlurb) {
+                    console.log('ERROR: data[i] is not instance of BlogPostBlurb');    
+                }
+
+                this.relatedPosts.push(data[i]);
+            }
+        });        
     }
 }
