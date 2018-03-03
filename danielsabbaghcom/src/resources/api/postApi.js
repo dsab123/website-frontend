12d73a58572fc9e1bca8413b21eca5e4c2d62267
dsab123 <dsabbaghumd@gmail.com>
2018-03-03 00:28:38 -0500

    basic hacky relatedPosts-related method temp code

--- danielsabbaghcom/src/resources/api/postApi.js
+++ danielsabbaghcom/src/resources/api/postApi.js
@@ -7,6 +7,8 @@ import {BlogPostBlurb} from '../models/blogPostBlurb';
 export class PostApi {
     constructor() {
         this.httpClient = new HttpClient();
+
+        this.numberOfBlogPostBlurbsToFetch = 5;
     }
     
     // method naming scheme - 'fetch' is prefix of any method
@@ -60,18 +62,55 @@ export class PostApi {
         let contents = '';
         await this.httpClient.fetch('https://baconipsum.com/api/?type=meat-and-filler')
             .then(response => {
-                console.log('got response');
                 contents = response.json();
             });        
 
         return contents; // should like to substring this
     }
 
-    async retrieveBlogPostsByTag(blogPostTag) {
+    async retrieveBlogPostBlurbsByTag(blogPostTag) {
+        let blogPostBlurbs = [];
+        //let contents = await this.fetchBlogPostBlurbsByTag(blogPostTag);
+        let contents = new Array(5);
 
+        for (let i = 0; i < contents.length; i++) {
+
+            // TODO when the back end is done, the marshaling will move upstream
+            // to the 'fetch' methods
+            blogPostBlurbs.push(new BlogPostBlurb({
+                id: i, 
+                title: `Fake Post ${i}`, 
+                teaser: "in a land far far away, where the back end is completed..."
+            }));
+
+            // TODO not sure how to resolve promise one by one, so I am doing this
+            // terrible thing. fix it
+            /*
+            blogPostBlurbs.push(new BlogPostBlurb({
+                id: i, 
+                title: `Fake Post ${i}`, 
+                teaser: contents[0].substr(0, 40) + "..."
+            }));
+            */
+        }
+    
+        return blogPostBlurbs;
     }
 
-    async fetchBlogPostsByTag(blogPostTag) {
+    async fetchBlogPostBlurbsByTag(blogPostTag) {
+        let contents = [];
 
+        for (let i = 0; i < this.numberOfBlogPostBlurbsToFetch; i++) {
+            await this.httpClient.fetch('https://baconipsum.com/api/?type=meat-and-filler')
+                .then(response => {
+                    contents.push(response.json());
+                });        
+        }
+
+        for (let i = 0; i < contents.length; i++) {
+            Promise.resolve(contents[i]);
+        }
+
+        return contents;
     }
 }
