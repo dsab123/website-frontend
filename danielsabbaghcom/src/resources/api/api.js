457d5e42f333ceea92b4ed0e834678fa336aa24e
dsab123 <dsabbaghumd@gmail.com>
2019-07-30 20:43:42 -0400

    add comment to handle baseUrl better

--- danielsabbaghcom/src/resources/api/api.js
+++ danielsabbaghcom/src/resources/api/api.js
@@ -6,6 +6,8 @@ export class Api {
     constructor() {
         this.httpClient = new HttpClient();
 
-        this.baseUrl = 'https://7dfaiqkhk5.execute-api.us-east-1.amazonaws.com/stage/';
+        this.baseUrl = 'https://7dfaiqkhk5.execute-api.us-east-1.amazonaws.com/stage/'; 
+        // should get this from config!
+        // and have some way of hiding this from snoopers
     }
 }
