7129ad8089e2504366618257944784319bf09a3e
dsab123 <dsabbaghumd@gmail.com>
2019-08-07 19:39:50 -0400

    remove markdown text from blurbs

--- danielsabbaghcom/src/resources/value-converters/blog-post-blurb-prettifier.js
+++ danielsabbaghcom/src/resources/value-converters/blog-post-blurb-prettifier.js
@@ -1,6 +1,20 @@
 export class BlogPostBlurbPrettifierValueConverter {
     toView(blurb) {
-        // for now, prettifying will be removing the last character (which could be a punctuation) and adding ellipses
+        // prettifying includes two things:
+        // 1 - removing any markdown inside the blurb
+        // remove all of #, !, *
+        blurb = blurb.replace(/[\#|\!\*]/g, '');
+        
+        // we'll also need to remove [][] and [](), as well as their contents
+        let matches = blurb.match(/(\[.*\])/g);
+        if (matches != null) {
+            matches.forEach(element => {
+                blurb = blurb.replace(element, '');
+            });
+        }
+
+        // 2 - trimm the last character in case its a markdown character,
+        // and add ellipses
         return `${blurb.slice(0, -1)}...`;;
     }
 }
