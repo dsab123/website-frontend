9bb90dd28212f364c2926b05c7903caeb5c708de
dsab123 <dsabbaghumd@gmail.com>
2019-12-02 21:04:35 -0500

    reduce number of requests made on home page

--- danielsabbaghcom/src/resources/components/home.js
+++ danielsabbaghcom/src/resources/components/home.js
@@ -16,7 +16,7 @@ export class Home {
 
         this.loadingTextTagline = "(why is this loading taking so long? click \'About\'!)"
 
-        this.numberOfRecentPostBlurbsToFetch = 10;
+        this.numberOfRecentPostBlurbsToFetch = 6;
     }
 
     sleep(milliseconds) {
