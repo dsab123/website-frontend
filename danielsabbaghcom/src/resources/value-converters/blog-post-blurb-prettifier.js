d48db9bc1160414cd8b6106dfa9f023ff9c95aea
dsab123 <dsabbaghumd@gmail.com>
2020-05-14 00:24:49 -0400

    simplify prettifier since post teasers are coming from database now

--- danielsabbaghcom/src/resources/value-converters/blog-post-blurb-prettifier.js
+++ danielsabbaghcom/src/resources/value-converters/blog-post-blurb-prettifier.js
@@ -1,20 +1,6 @@
 export class BlogPostBlurbPrettifierValueConverter {
     toView(blurb) {
-        // prettifying includes two things:
-        // 1 - removing any markdown inside the blurb
-        // remove all of #, !, *
-        blurb = blurb.replace(/[\#|\!\*]/g, '');
-        
-        // we'll also need to remove [][] and [](), as well as their contents
-        let matches = blurb.match(/(\[.*\])/g);
-        if (matches != null) {
-            matches.forEach(element => {
-                blurb = blurb.replace(element, '');
-            });
-        }
-
-        // 2 - trimm the last character in case its a markdown character,
-        // and add ellipses
-        return `${blurb.slice(0, -1)}...`;;
+        // prettification is simply just ellipses
+        return `${blurb}...`;;
     }
 }
