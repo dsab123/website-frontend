00a5cbeae0844e6eee42faa21fd1942d5a3e365e
dsab123 <dsabbaghumd@gmail.com>
2020-05-22 13:30:44 -0400

    got paypal donate button working

--- danielsabbaghcom/src/resources/components/footer.js
+++ danielsabbaghcom/src/resources/components/footer.js
@@ -1,8 +1,5 @@
 export class Footer {
     constructor() {
-        // get these from some config or something
-        this.blurbText = "Adventures of a software developer who has climbed up the stack from C to Javascript; currently doing cool full-stack things.";
-        this.email = "sampleEmail@email.com";
+        this.blurbText = "Adventures of a software developer who has climbed up the stack from C to Javascript; currently doing cool full-stack things. dsabbaghumd AT gmail.";
     }
-
 }
