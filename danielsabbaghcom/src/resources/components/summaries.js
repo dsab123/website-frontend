4cce25a1bcf010bf0da1cc444c49178c3905b4a4
dsab123 <dsabbaghumd@gmail.com>
2020-08-23 18:28:21 -0400

    added money plea to summaries page

--- danielsabbaghcom/src/resources/components/summaries.js
+++ danielsabbaghcom/src/resources/components/summaries.js
@@ -8,6 +8,7 @@ export class Summaries {
 
         this.intro = "I like to read pretty widely, from tech to theology to bestsellers. Below you'll find a review for each of the books I've read, along with a link to buy. Drop me a line if you purchase any of these!";
         this.disclaimer = "I'm a participant in the Amazon Services LLC Associates Program, an affiliate advertising program designed to provide a means for sites to earn advertising fees by advertising and linking to Amazon.";
+        this.moneyPlea = "If you have found this content helpful, please consider supporting my work through paypal by clicking on the coffee cup at the bottom of this page."
         this.summaries = [];
         this.flag = false;
     }
